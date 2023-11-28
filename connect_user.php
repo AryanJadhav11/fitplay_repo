02f665cd5c65c404fadf353b8c04c311bfab29bc
AryanJadhav <jadhavaryan467@gmail.com>
2023-11-29 02:32:51 +0530

    wer

--- connect_user.php
+++ connect_user.php
@@ -1,28 +1,31 @@
 <?php
-$fname=$_POST['firstname'];
-$lname=$_POST['lastname'];
-$uname=$_POST['username'];
-$email=$_POST['email'];
-$password=$_POST['password'];
+$fname = $_POST['firstname'];
+$lname = $_POST['lastname'];
+$uname = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
 
-//connection 
-$conn=new mysqli('localhost','root','','fitplay_users');
-if($conn->connect_error)
-{
-    die('connection failer')
-}
-else{
-    $stmt=$conn->prepare("insert into users(firstname, lastname, username, email, password)
-    values(?,?,?,?,?)");
-    //bind the paramters to the placeholders in the query
-    $stmt->bind_param('sssss',$fname,$lname,$uname,$email,$password);
+// Connection
+$conn = new mysqli('localhost', 'root', '', 'fitplay_users');
+if ($conn->connect_error) {
+    die('Connection failure: ' . $conn->connect_error); // Fixed syntax error here
+} else {
+    $stmt = $conn->prepare("INSERT INTO users (firstname, lastname, username, email, password)
+                            VALUES (?, ?, ?, ?, ?)");
+
+    // Bind the parameters to the placeholders in the query
+    $stmt->bind_param('sssss', $fname, $lname, $uname, $email, $password);
+
     if ($stmt->execute()) {
-        header("Location:index.html");
-    }
-    else
-    {
-        echo("try again");
+        header("Location: index.html");
+    } else {
+        echo "Try again";
     }
 
+    // Close the prepared statement
+    $stmt->close();
+    
+    // Close the database connection
+    $conn->close();
 }
 ?>
