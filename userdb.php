738d0918de5766db88ed1b5fc7cf2b283c5c9ee1
AryanJadhav <jadhavaryan467@gmail.com>
2023-11-29 03:11:19 +0530

    ees

--- userdb.php
+++ userdb.php
@@ -1,32 +1,41 @@
-
-
 <?php
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    // Database connection
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "fitplay_users";
 
-$showalert=false;
-$showerr=false;
-$showcharerr=false;
-// Connection
-include 'dbconnectuser.php';
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
-	
-	$firstname=$_POST["fname"];
-	$lastname=$_POST["lname"];
-    $username=$_POST["uname"];
-	$email=$_POST["mail"];
-	$password=$_POST["password"];
-	
-	$sql = "INSERT INTO `users` ( `firstname`, `lastname`, `username`, `email`, `password`) VALUES ('$firstname', '$lastname', ' $username', '$email', '$password' );";
-  $result = mysqli_query($conn, $sql); 
-	if($result)
-  {
-    $showalert=true;
-  }
-  {
-    $showerr="something went wrong";
-  }
-	
+    // Fetch user details from the form
+    $firstname = $_POST["fname"];
+    $lastname = $_POST["lname"];
+    $username = $_POST["uname"];
+    $email = $_POST["mail"];
+    $password = $_POST["password"];
+
+    // SQL query to insert data into the users table
+    $sql = "INSERT INTO users (firstname, lastname, username, email, password)
+            VALUES ('$firstname', '$lastname', '$username', '$email', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        // Redirect to a success page
+        header("Location: success.html");
+        exit();
+    } else {
+        // Handle errors
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    // Close the database connection
+    $conn->close();
 }
 ?>
