cd14fd377a9c297c43e9c573e87962285dd980c0
Amitkondami <109058496+Amitkondami@users.noreply.github.com>
2024-02-08 21:59:31 +0530

    hello

--- turf.php
+++ turf.php
@@ -150,7 +150,7 @@ function getInitials($name) {
 
 /* Section - Title */
 /**************************/
-#parallax-world-of-ugg .title {background: white; padding: 60px; margin:0 auto; text-align:center;}
+#parallax-world-of-ugg .title {background: white; padding:10px; margin:0 auto; text-align:center;}
 #parallax-world-of-ugg .title h1 {font-size:35px; letter-spacing:8px;}
 
 /* Section - Block */
@@ -162,7 +162,7 @@ function getInitials($name) {
 /* Section - Parallax */
 /**************************/
 #parallax-world-of-ugg .parallax-one {padding-top: 200px; padding-bottom: 200px; overflow: hidden; position: relative; width: 100%; background-image: url(https://www.wallpaperup.com/uploads/wallpapers/2013/08/05/128479/7dcfba828f21ff03a7ac12daf63d6abe.jpg); background-attachment: fixed; background-size: cover; -moz-background-size: cover; -webkit-background-size: cover; background-repeat: no-repeat; background-position: top center;}
-#parallax-world-of-ugg .parallax-two {padding-top: 200px; padding-bottom: 200px; overflow: hidden; position: relative; width: 100%; background-image: url(https://th.bing.com/th/id/R.a2416d19e9fcb909448cc8ed42e28b96?rik=ji7Sz%2fgimEgFHA&riu=http%3a%2f%2fwww.atozpictures.com%2fuploads%2f2016%2f03%2fvirat-kohli-t20-world-cup-50-against-pakistan-at-2016.jpg&ehk=KF%2bcprOQNgD98Np1EQcGRMCPyyk%2bwPvChdkX5mFen%2bI%3d&risl=&pid=ImgRaw&r=0); background-attachment: fixed; background-size: cover; -moz-background-size: cover; -webkit-background-size: cover; background-repeat: no-repeat; background-position: center center;}
+#parallax-world-of-ugg .parallax-two {padding-top: 200px; padding-bottom: 200px; overflow: hidden; position: relative; width: 100%; background-image: url(https://png.pngtree.com/thumb_back/fw800/background/20230527/pngtree-gym-is-reflected-in-an-odd-light-image_2675068.jpg); background-attachment: fixed; background-size: cover; -moz-background-size: cover; -webkit-background-size: cover; background-repeat: no-repeat; background-position: center center;}
 #parallax-world-of-ugg .parallax-three {padding-top: 200px; padding-bottom: 200px; overflow: hidden; position: relative; width: 100%; background-image: url(https://th.bing.com/th/id/R.ae120d27c9edd5524193b709336c8c25?rik=K1nTQEniyptD7Q&riu=http%3a%2f%2fwww.hdcarwallpapers.com%2fwalls%2fhonda_f1_racing_car-wide.jpg&ehk=4v%2fReKKyhgUflpCDe8xPytG%2b%2bxNMTc8oZYOIIuIYkSw%3d&risl=&pid=ImgRaw&r=0); background-attachment: fixed; background-size: cover; -moz-background-size: cover; -webkit-background-size: cover; background-repeat: no-repeat; background-position: center center;}
 
 /* Extras */
@@ -375,7 +375,46 @@ function getInitials($name) {
     </div>
   </div>
 </div>
-
+<!-- parallax start -->
+<div>
+<div id="parallax-world-of-ugg">
+  
+  <section>
+    <div class="title">
+      <h3>FIT</h3>
+      <h1 style="color:green">PLAY</h1>
+    </div>
+  </section>
+  
+  <section>
+      <div class="parallax-one">
+        <h2>TURFS</h2>
+      </div>
+  </section>
+  
+  <section>
+    <div class="block">
+      <p><span class="first-character sc">I</span>n 1978, Brian Smith landed in Southern California with a bag of sheepskin boots and hope. He fell in love with the sheepskin experience and was convinced the world would one day share this love. The beaches of Southern California had long been an epicenter of a relaxed, casual lifestyle, a lifestyle that Brian felt was a perfect fit for his brand. So he founded the UGG brand, began selling his sheepskin boots and they were an immediate sensation. By the mid 1980's, the UGG brand became a symbol of relaxed southern California culture, gaining momentum through surf shops and other shops up and down the coast of California, from San Diego to Santa Cruz. UGG boots reached beyond the beach, popping up in big cities and small towns all over, and in every level of society.</p>
+      <p class="line-break margin-top-10"></p>
+    </div>
+  </section>
+  
+  <section>
+    <div class="parallax-two">
+      <h2>GYMS</h2>
+    </div>
+  </section>
+  
+  <section>
+    <div class="block">
+      <p><span class="first-character ny">B</span>reaking into the New York fashion world is no easy task. But by the early 2000's, UGG Australia began to take it by storm. The evolution of UGG from a brand that made sheepskin boots, slippers, clogs and sandals for an active, outdoor lifestyle to a brand that was now being touted as a symbol of a stylish, casual and luxurious lifestyle was swift. Much of this was due to a brand repositioning effort that transformed UGG into a high-end luxury footwear maker. As a fashion brand, UGG advertisements now graced the pages of Vogue Magazine as well as other fashion books. In the mid 2000's, the desire for premium casual fashion was popping up all over the world and UGG was now perfectly aligned with this movement.</p>
+      <p class="line-break margin-top-10"></p>
+    </div>
+  </section>
+    
+  </div>
+</div>
+<!-- prallex end -->
 
 
   <div id="hero-carousel" class="carousel slide  carousel-fade" data-bs-ride="carousel" style="margin-bottom:10px;">
@@ -509,60 +548,6 @@ $resimg=mysqli_fetch_assoc($que2);
     });
 </script>
 <!-- parallex -->
-<div>
-<div id="parallax-world-of-ugg">
-  
-  <section>
-    <div class="title">
-      <h3>LETS</h3>
-      <h1>PLAY</h1>
-    </div>
-  </section>
-  
-  <section>
-      <div class="parallax-one">
-        <h2>FOOTBALL</h2>
-      </div>
-  </section>
-  
-  <section>
-    <div class="block">
-      <p><span class="first-character sc">I</span>n 1978, Brian Smith landed in Southern California with a bag of sheepskin boots and hope. He fell in love with the sheepskin experience and was convinced the world would one day share this love. The beaches of Southern California had long been an epicenter of a relaxed, casual lifestyle, a lifestyle that Brian felt was a perfect fit for his brand. So he founded the UGG brand, began selling his sheepskin boots and they were an immediate sensation. By the mid 1980's, the UGG brand became a symbol of relaxed southern California culture, gaining momentum through surf shops and other shops up and down the coast of California, from San Diego to Santa Cruz. UGG boots reached beyond the beach, popping up in big cities and small towns all over, and in every level of society.</p>
-      <p class="line-break margin-top-10"></p>
-      <p class="margin-top-10">The UGG brand began to symbolize those who embraced sport and a relaxed, active lifestyle. More than that, an emotional connection and a true feeling of love began to grow for UGG boots, just as Brian had envisioned. People didn't just like wearing UGG boots, they fell in love with them and literally could not take them off. By the end of the 90's, celebrities and those in the fashion world took notice of the UGG brand. A cultural shift occurred as well - people were embracing, and feeling empowered, by living a more casual lifestyle and UGG became one of the symbols of this lifestyle. By 2000, a love that began on the beaches had become an icon of casual style. It was at this time that the love for UGG grew in the east, over the Rockies and in Chicago.</p>
-    </div>
-  </section>
-  
-  <section>
-    <div class="parallax-two">
-      <h2>CRICKET</h2>
-    </div>
-  </section>
-  
-  <section>
-    <div class="block">
-      <p><span class="first-character ny">B</span>reaking into the New York fashion world is no easy task. But by the early 2000's, UGG Australia began to take it by storm. The evolution of UGG from a brand that made sheepskin boots, slippers, clogs and sandals for an active, outdoor lifestyle to a brand that was now being touted as a symbol of a stylish, casual and luxurious lifestyle was swift. Much of this was due to a brand repositioning effort that transformed UGG into a high-end luxury footwear maker. As a fashion brand, UGG advertisements now graced the pages of Vogue Magazine as well as other fashion books. In the mid 2000's, the desire for premium casual fashion was popping up all over the world and UGG was now perfectly aligned with this movement.</p>
-      <p class="line-break margin-top-10"></p>
-      <p class="margin-top-10">Fueled by celebrities from coast to coast wearing UGG boots and slippers on their downtime, an entirely new era of fashion was carved out. As a result, the desire and love for UGG increased as people wanted to go deeper into this relaxed UGG experience. UGG began offering numerous color and style variations on their sheepskin boots and slippers. Cold weather boots for women and men and leather casuals were added with great success. What started as a niche item, UGG sheepskin boots were now a must-have staple in everyone's wardrobe. More UGG collections followed, showcasing everything from knit boots to sneakers to wedges, all the while maintaining that luxurious feel UGG is known for all over the world. UGG products were now seen on runways and in fashion shoots from coast to coast. Before long, the love spread even further.</p>
-    </div>
-  </section>
-  
-  <section>
-    <div class="parallax-three">
-      <h2>F1 RACING</h2>
-    </div>
-  </section>
-  
-  <section>
-    <div class="block">
-      <p><span class="first-character atw">W</span>hen the New York fashion community notices your brand, the world soon follows. The widespread love for UGG extended to Europe in the mid-2000's along with the stylish casual movement and demand for premium casual fashion. UGG boots and shoes were now seen walking the streets of London, Paris and Amsterdam with regularity. To meet the rising demand from new fans, UGG opened flagship stores in the UK and an additional location in Moscow. As the love spread farther East, concept stores were opened in Beijing, Shanghai and Tokyo. UGG Australia is now an international brand that is loved by all. This love is a result of a magical combination of the amazing functional benefits of sheepskin and the heightened emotional feeling you get when you slip them on your feet. In short, you just feel better all over when you wear UGG boots, slippers, and shoes.</p>
-      <p class="line-break margin-top-10"></p>
-      <p class="margin-top-10">In 2011, UGG will go back to its roots and focus on bringing the active men that brought the brand to life back with new styles allowing them to love the brand again as well. Partnering with Super Bowl champion and NFL MVP Tom Brady, UGG will invite even more men to feel the love the rest of the world knows so well. UGG will also step into the world of high fashion with UGG Collection. The UGG Collection fuses the timeless craft of Italian shoemaking with the reliable magic of sheepskin, bringing the luxurious feel of UGG to high end fashion. As the love for UGG continues to spread across the world, we have continued to offer new and unexpected ways to experience the brand. The UGG journey continues on and the love for UGG continues to spread.</p>
-    </div>
-  </section>
-    
-  </div>
-</div>
 <!-- parallex -->
 <div class="banner-containerw">
   <img src="one.png" style="width:100%; height:240px;">
