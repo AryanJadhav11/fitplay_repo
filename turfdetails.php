54241e6e51813f7118100521dca46cdbcaeb9793
Amitkondami <109058496+Amitkondami@users.noreply.github.com>
2024-02-08 18:42:15 +0530

    hhh

--- turfdetails.php
+++ turfdetails.php
@@ -40,12 +40,7 @@ $end_time_12hr = date("h:i A", strtotime($row9['end']));
 
   <!-- icon cdn -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-
-  <!-- bootstrap cdn links -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-
+  
   <!-- Google Fonts -->
   <link
     href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
@@ -58,10 +53,6 @@ $end_time_12hr = date("h:i A", strtotime($row9['end']));
   <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
   <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css'>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'></script> 
-<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/js/bootstrap.min.js'></script>
 
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
@@ -99,195 +90,209 @@ $end_time_12hr = date("h:i A", strtotime($row9['end']));
     <div class="container d-flex align-items-center justify-content-between">
 
       <h1 class="logo"><a href="index.html">Fit<span style="color: rgb(14, 198, 14);">.play</span></a></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt=""></a>-->
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto active" href="#hero">Shop</a></li>
           <li><a class="nav-link scrollto" href="#services">Services</a></li>
-          <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
-          <li><a class="nav-link scrollto" href="#team">Team</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">Contact</a></li>
+          <button class="btn" style="background-color: rgb(13, 110, 253); margin-left: 30px;">Sign UP</button>
+          <button class="btn" style="background-color: green; margin-left: 30px;">Log In</button>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
-
     </div>
   </header><!-- End Header -->
-  <br>
-  <br>
 
+
+
+  
   <!-- carosal start  -->
-
-
   <div class="container">
     <div class="row">
       <div class="col-lg-8">
-      <h2><?= ucfirst($row9['name']) ?></h2>
-        <p><?= ucfirst($row9['place']) ?></p>
-        <p><?= ucfirst($row9['price']) ?></p>
-
+        <h2 style="font-family:'Protest Strike', sans-serif;";><b><?= ucfirst($row9['name']) ?></b></h2>
+        <h3><b><?= ucfirst($row9['place']) ?></b><h3>
+        <h5><b>₹<?= ucfirst($row9['price']) ?></b></h5>
       </div>
-      <div class="col-lg-4">
-        <br>
 
-      <div class="p-3" style="border-radius: 10px; background-color: rgb(217, 217, 217); box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.5);">
-        <button class="btn">
-        <a href="bookturf.php?id=<?= $row9['id'] ?>">Book Now</a>
-        </button>
-      </div>  
+      <div class="row">
+        <div class="col-lg-8">
+          <div class="border">
+          <?php $imgi=$row9['image']?>
+          <img src="upload/<?=$imgi?>" style="height:500px; width:850px ;border-radius:30px;">
+          </div>
+        
         </div>
-        <div class="border" style=":red">
-        <button class="btn w-100 h-50 bg-success text-white">
-        <a href="bookturf.php?id=<?= $row9['id'] ?>" class="btn btn-primary">Book Now</a>
-        </button>
-        </div>
-       
-      </div>
-    </div>
-<h1>hello aryan</h1>
-    <div class="row">
-      <div class="col-lg-8">
-        <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel"
-          style="border: 1px solid black; border-radius: 15px; overflow: hidden;">
-          <div class="carousel-inner">
-            <div class="carousel-item active" data-bs-interval="2000">
-            <?php $imgi=$row9['image']?>
-          <img src="upload/<?=$imgi?>" >
+        <div class="col-lg-4">
+          <div class="p-3"
+            style="border-radius: 10px; background-color: rgb(223, 219, 219); justify-content:center">
+            <a href="bookturf.php?id=<?= $row9['id'] ?>">
+            <button class="btn h-60 bg-success text-white"
+              style="margin-bottom: 10px; width: 320px; border-radius: 100px; ">
+              Book Now
+            </button>
+            </a>
+
+            <button class="btn w-50 bg-primary text-white"
+              style="margin-left: 80px; margin-top: 10px; border-radius: 100px; ">
+              <i class="fas fa-share-alt"></i> SHARE
+            </button>
+            <br>
+            <br>
+            <div class="border"
+              style="border-radius: 10px; background-color: rgb(223, 219, 219); ">
+              <h3 style="text-size:40px">Timming</h3>
+              <h6 style="padding-top: 10px; padding-left: 10px;">From:<?= $start_time_12hr; ?></h6>
+              <h6><p style="padding-left: 10px; padding-top:10px">To:<?= $end_time_12hr; ?></p></h6>
+            </div>
+            <br>
+            <h3><b>Location</b></h3 style="padding:20px">
+            <p><?= ucfirst($row9['place']) ?></p>
+            <div class="w-20 h-20">
+            <?= ucfirst($row9['loc']) ?>
+            </div>
             </div>
           </div>
-          
         </div>
-      </div>
+        <div class="row">
+          <div class="col-lg-8">
+            <div class="border"
+              style="border-radius: 10px; margin-top: 20px; background-color: rgb(217, 217, 217); box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.5);">
+              <div class="" style="margin-top: 10px; border-radius: 10px;margin-bottom: 10px;">
+                <div class="align-items-center justify-content-center">
+                  <button class=" btn btn-danger mt-3"
+                  style="margin-left: 60px; margin-bottom: 10px; border-radius: 50px; width: 150px; height: 50px;">
+                  <i class="fas fa-futbol" style="size: 50px;"></i>
+                  Football
+                  </button>
+                  <button class="btn btn-danger mt-3"
+                    style="margin-left: 60px; margin-bottom:10px; border-radius: 50px; width: 150px; height: 50px;">
+                    <i class="fas fa-baseball-ball"></i> Cricket
+                  </button>
+                  <button class="btn btn-danger mt-3"
+                    style="margin-left: 60px; margin-bottom:10px; border-radius: 50px; width: 150px; height: 50px;">
+                    <i class="fas fa-baseball-ball"></i> Tennis
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-8">
+            <div class="border"
+              style="background-color:rgb(217, 217, 217) ;border-radius: 10px; margin: 30px; box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.5);">
+              <div class="border" style="background-color: white; border-radius: 10px; margin: 20px;">
+                <h4 style="padding: 10px; padding-bottom: 0px;"><b>SOME STUFF OF FOOTBAL</b></h4>
+                <p style="padding: 10px;">My Favorite Game (Football) ! Football is my favorite game because it is quite
+                  challenging and
+                  interesting game. I began to play football while I was 6 years old. Since then it has become my
+                  passion.
+                  It is also called “king sport” and is the most famous sport in the entire world.</p>
+              </div>
+            </div>
+          </div>
+        </div>
 
-      <div class="col-lg-4">
-        <div class="d-flex">
-          <button class="btn w-100 h-50 bg-success text-white" style="border-radius">
-            <i class="fas fa-share-alt"></i> SHARE
-          </button>
-         
-        </div>
-        <br>
-        <div class=" border border-secondary p-3" style="border-radius: 10px;">
-          <h4><b>Timing</b></h4>
-          <p><b>From:</b> <?= $start_time_12hr; ?></p>
-          <p><b>To:</b> <?= $end_time_12hr; ?></p>
-        </div>
-        <br>
-        <div class=" border border-secondary p-3" style="border-radius: 10px;">
-          <h3>Location</h3>
-          <p><?= ucfirst($row9['place']) ?></p>
-          <?= ucfirst($row9['loc']) ?>
 
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-lg-8 border border-secondary" style="border-radius: 10px; height: 100px;">
-        <h4 class="pt-3"><b>Sports Available</b></h4>
-        <div>
-          <button class="btn btn-outline-danger">
-            <i class="fas fa-futbol" style="size: 50px;"></i>
-            Football
-          </button>
-          <button class="btn btn-outline-danger">
-            <i class="fas fa-baseball-ball"></i> Cricket
-          </button>
-        </div>
-      </div>
-    </div>
-    <br>
-    <div class="row">
-      <div class="col-lg-8 border border-secondary" style="border-radius: 10px; height: 100px;">
-        <h4 class="pt-3" style="font-size: x-large;"><b>Amenities</b></h4>
-        <div>
-        <p><?= ucfirst($row9['amenitiy']) ?></p>
+        <div class="row">
+          <div class="col-lg-8 border"
+            style="border-radius: 10px; background-color:rgb(217, 217, 217); margin: 20p;box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.5);">
+            <div class="border" style="background-color: white; border-radius: 10px; margin: 10px;">
+              <h4 class="p-2" style="font-size: x-large;"><b>Amenities</b></h4>
+              <div class="p-2">
+                <div>
+                  <div>
+                   <h6 style=""><b><?= ucfirst($row9['amenitiy']) ?></b></h6> 
+                  </div>
+                <p></p>
+                </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
   </div>
+  </div>
+  </div>
+  </div>
+  </section>
+  </div>
+  </div>
   <br>
   <br>
   <br>
-
   <!-- carosal end -->
 
-  
-
   <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
 
-    
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>BizLand<span>.</span></h3>
+            <p>
+              A108 Adam Street <br>
+              New York, NY 535022<br>
+              United States <br><br>
+              <strong>Phone:</strong> +1 5589 55488 55<br>
+              <strong>Email:</strong> info@example.com<br>
+            </p>
+          </div>
 
-<div class="footer-top">
-  <div class="container">
-    <div class="row">
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+            </ul>
+          </div>
 
-      <div class="col-lg-3 col-md-6 footer-contact">
-        <h3>FitPlay.<span>.</span></h3>
-        <p>
-         Kolhapur <br>
-         Maharastra<br>
-          <strong>Phone:</strong> +91 9284008321<br>
-          <strong>Email:</strong> thefitplay@gmail.com<br>
-        </p>
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+            </ul>
+          </div>
 
-      </div>
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Social Networks</h4>
+            <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
+            <div class="social-links mt-3">
+              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            </div>
+          </div>
 
-      <div class="col-lg-3 col-md-6 footer-links">
-        <h4>Useful Links</h4>
-        <ul>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-          
-          <li><i class="bx bx-chevron-right"></i> <a href="privacy_policy.html">Privacy policy</a></li>
-        </ul>
-      </div>
-
-      <div class="col-lg-3 col-md-6 footer-links">
-        <h4>Our Services</h4>
-        <ul>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Book Turfs</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Explore Gym</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Shop Products</a></li>
-          
-        </ul>
-      </div>
-
-
-
-      <div class="col-lg-3 col-md-6 footer-links">
-        <h4>Our Social Networks</h4>
-        <p>Welcome to the heart of our vibrant community! Follow us on our social networks to stay connected with the latest in fitness trends, exciting events, exclusive promotions, and inspiring stories from our community members.</p>
-        <div class="social-links mt-3">
-          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-          <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
         </div>
-
       </div>
-      
-
     </div>
-  </div>
-</div>
 
-<div class="container py-4">
-  <div class="copyright">
-    &copy; Copyright <strong><span>FitPlay</span></strong>. All Rights Reserved
-  </div>
-  <div class="credits">
-    <!-- All the links in the footer should remain intact. -->
-    <!-- You can delete the links only if you purchased the pro version. -->
-    <!-- Licensing information: https://bootstrapmade.com/license/ -->
-    <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bizland-bootstrap-business-template/ -->
-    Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-  </div>
-</div>
-</footer><!-- End Footer -->
-  
+    <div class="container py-4">
+      <div class="copyright">
+        &copy; Copyright <strong><span>BizLand</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bizland-bootstrap-business-template/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
 
   <div id="preloader"></div>
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
