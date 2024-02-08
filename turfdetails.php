f0b6ccdae07d98736e341e0271b7d8b1344e2d8b
Amitkondami <109058496+Amitkondami@users.noreply.github.com>
2024-02-08 16:10:17 +0530

    ghjk

--- turfdetails.php
+++ turfdetails.php
@@ -41,6 +41,9 @@ $end_time_12hr = date("h:i A", strtotime($row9['end']));
   <!-- icon cdn -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
+  <!-- bootstrap cdn links -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 
   <!-- Google Fonts -->
@@ -129,12 +132,15 @@ $end_time_12hr = date("h:i A", strtotime($row9['end']));
       </div>
       <div class="col-lg-4">
         <br>
-        <button class="btn w-100 h-50 bg-success text-white">
-        <a href="bookturf.php?id=<?= $row9['id'] ?>" class="btn btn-primary">Book Now</a>
+      <div class="p-3" style="border-radius: 10px; background-color: rgb(217, 217, 217); box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.5);">
+        <button class="btn">
+        <a href="bookturf.php?id=<?= $row9['id'] ?>">Book Now</a>
         </button>
+      </div>  
+        </div>
       </div>
     </div>
-
+<h1>hello aryan</h1>
     <div class="row">
       <div class="col-lg-8">
         <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel"
