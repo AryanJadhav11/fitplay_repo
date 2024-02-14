e1783a18f144e04ce7136da55ca91b956df6bc4d
Amitkondami <109058496+Amitkondami@users.noreply.github.com>
2024-02-14 16:19:14 +0530

    shopui

---
+++ shopsample.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <!-- cdn links -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<?php include("header.php");?>
+<!-- carosal start -->
+<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="https://contents.mediadecathlon.com/s1060328/k$551476c484457458609a90f62a1988c2/99%20under%20999%20%20web.jpg?format=auto&quality=70&f=1480x0" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="https://contents.mediadecathlon.com/s1051803/k$24a0285566a6d9ea9d53a2bde53a3b02/Sale%20%20web.jpg?format=auto&quality=70&f=1480x0" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://contents.mediadecathlon.com/s1061445/k$b0d33a4289de7edac9953bdbd35d99cc/Frame%20427321401.png?format=auto&quality=70&f=1480x0" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<!-- carosal end -->
+<br>
+<br>
+<br>
+<br>
+<!-- facilities start -->
+<div class="container">
+	<div class="row  text-center">
+		<div class="col-sm-4 col-md-4 facility-cod">
+			<h3> Cash On Delivery </h3>
+			<p> Lorem ipsum dolor sit amet, novum paulo principes ut eam, omnium graecis inimicus cum ne. Pro nusquam suscipiantur ei, ad vel dolorem consetetur, impetus lucilius vulputate vim te. Saperet ancillae officiis no est, wisi intellegat id eam, has omnes expetendis theophrastus eu. </p>
+		</div>
+		<div class="col-sm-4 col-md-4 facility-shipping">
+			<h3> Free Shipping</h3>
+			<p>Lorem ipsum dolor sit amet, novum paulo principes ut eam, omnium graecis inimicus cum ne. Pro nusquam suscipiantur ei, ad vel dolorem consetetur, impetus lucilius vulputate vim te. Saperet ancillae officiis no est, wisi intellegat id eam, has omnes expetendis theophrastus eu. </p>
+		</div>
+		<div class="col-sm-4 col-md-4 facility-returns">
+			<h3>Easy Returns </h3>
+			<p>Lorem ipsum dolor sit amet, novum paulo principes ut eam, omnium graecis inimicus cum ne. Pro nusquam suscipiantur ei, ad vel dolorem consetetur, impetus lucilius vulputate vim te. Saperet ancillae officiis no est, wisi intellegat id eam, has omnes expetendis theophrastus eu. </p>
+		</div>
+	</div>
+</div>
+
+<style>
+  .facility-cod
+{
+	background:url("http://i345.photobucket.com/albums/p396/ruchi122/1470920870_07Wallet_zpsswjtv8dx.png") no-repeat top;
+	padding-top:70px;
+}
+
+.facility-cod:hover
+{
+	background:url("http://i345.photobucket.com/albums/p396/ruchi122/1470920870_07_zpspz41uc4k.png") no-repeat top;
+}
+
+.facility-shipping
+{	background:url("http://i345.photobucket.com/albums/p396/ruchi122/1471024355_ic_local_shipping_48px_zpslmejbm3l.png") no-repeat top;
+	padding-top:70px;
+}
+
+.facility-shipping:hover
+{	background:url("http://i345.photobucket.com/albums/p396/ruchi122/1470920793_ic_local_shipping_48px_zpsijbwmkjd.png") no-repeat top;
+}
+
+.facility-returns
+{
+	background:url("http://i345.photobucket.com/albums/p396/ruchi122/1471024335_return_zps5wptjjcf.png") no-repeat top;
+	padding-top:70px;
+}
+
+.facility-returns:hover
+{
+	background:url("http://i345.photobucket.com/albums/p396/ruchi122/1470920778_return_zpswu9lrlui.png") no-repeat top;
+}
+
+/*end of facilities*/
+</style>
+<!-- facilities end -->
+<br>
+<br>
+<br>
+<!-- image gallary start -->
+<div class="container">
+<h3>TOP DETAILS</h3>
+            <div class="row">
+                <div class="col-md-4 mt-3 col-lg-4">
+                    <img src="https://contents.mediadecathlon.com/p2314499/f1c060d2d60eabe474fb5eae1949a144/p2314499.jpg?format=auto&quality=70&f=1500x900" class="img-fluid" alt="image">
+                </div>
+                <div class="col-md-4 mt-3 col-lg-4">
+                    <img src="https://contents.mediadecathlon.com/p2407364/k$c5318a11a9f71ea9e8ad340acc1dc47b/plastic-cricket-stump-set-turquoise.jpg?format=auto&quality=70&f=1500x900" class="img-fluid" alt="image">
+                </div>
+                <div class="col-md-4 mt-3 col-lg-4">
+                    <img src="https://contents.mediadecathlon.com/p2579294/k$a5be54d00ab5e50c33dfbdba1f770007/men-s-mountaineering-down-jacket-mountaineering-light-grey.jpg?format=auto&quality=70&f=1500x900" class="img-fluid" alt="image">
+                </div>
+                <div class="col-md-4 mt-3 col-lg-6">
+                    <img src="https://contents.mediadecathlon.com/p1827551/k$682935e4f07fe2b685749090943120d4/hex-dumbbell-225-kg-black.jpg?format=auto&quality=70&f=1500x900" class="img-fluid" alt="image">
+                </div>
+                <div class="col-md-4 mt-3 col-lg-6">
+                    <img src="https://contents.mediadecathlon.com/p2071959/k$aa5b68c80c590ba1db35c3294975f0db/women-s-trackpant-for-gym-cotton-rich-500-black.jpg?format=auto&quality=70&f=1500x600" class="img-fluid" alt="image">
+                </div>
+                <div class="col-md-4 mt-3 col-lg-3">
+                    <img src="https://contents.mediadecathlon.com/p2412797/k$4d737e8e6056941e8c44b5edc595a202/kids-cricket-tennis-ball-cricket-bat-t100-easy-blue.jpg?format=auto&quality=70&f=1500x900" class="img-fluid" alt="image">
+                </div>
+                <div class="col-md-4 mt-3 col-lg-3">
+                    <img src="https://contents.mediadecathlon.com/p2239229/k$eebfc19ce3ad5a218eaa97f06fdcfa1d/all-weather-ultralight-plastic-cricket-set-cst-500.jpg?format=auto&quality=70&f=1500x900" class="img-fluid" alt="image">
+                </div>
+                <div class="col-md-4 mt-3 col-lg-3">
+                    <img src="https://contents.mediadecathlon.com/p2606718/k$eccde2c55be4eeae078f19b0f920fca1/football-club-ball-size-5-fifa-basic-f500-white-yellow.jpg?format=auto&quality=70&f=1500x900" class="img-fluid" alt="image">
+                </div>
+                <div class="col-md-4 mt-3 col-lg-3">
+                    <img src="https://contents.mediadecathlon.com/p2606820/k$2ac06f4bf3b9ef1dc625da121c9af29f/kids-football-goalkeeper-gloves-f100-black-yellow.jpg?format=auto&quality=70&f=1500x900" class="img-fluid" alt="image">
+                </div>
+
+            </div>
+        </div>
+<!-- image gallary end -->
+<br>
+<br>
+<br>
+<br>
+<!-- whats new start -->
+<!--new product-->
+<div class="container whats-new">
+  <div class="row">
+  <div class="col-sm-6 col-md-6 col-lg-6 sofa-float text-center">
+		<img src="https://th.bing.com/th/id/OIP.cOZL6tqhZXslkRe66O3-YwHaJz?pid=ImgDet&w=190&h=252&c=7&dpr=1.1" class="img-responsive">
+	</div>
+	<div class="col-sm-6 col-md-6 text-center whats-new-text">
+		<h1>Check out our Turf Platform</h1>
+		<p class="paragraphs"> Lorem ipsum dolor sit amet, novum paulo principes ut eam, omnium graecis inimicus cum ne. Pro nusquam suscipiantur ei, ad vel dolorem consetetur, impetus lucilius vulputate vim te. Saperet ancillae officiis no est, wisi intellegat id eam, has omnes expetendis theophrastus eu. Eam et mazim doctusLorem ipsum dolor sit amet, novum paulo principes ut eam, omnium graecis inimicus cum ne.     </p>
+	</div>
+  </div>
+	
+</div>
+<!--new product-->
+<style>
+.whats-new
+{
+	margin-top:40px;
+}
+
+.sofa-float
+{
+	position: relative;
+	display:block;
+	max-width:400px;
+	margin:0px auto;
+	animation:animatedsofa 3s infinite;
+}
+
+@keyframes animatedsofa
+{
+	0%{transform:translateY(10px);}
+	50%{transform:translateY(-20px);}
+	100%{transform:translateY(10px);}
+}
+
+.paragraphs
+{
+	max-width:400px;
+	max-height:400px;
+	margin:0px auto;
+	display: block;
+}
+
+.whats-new-text
+{
+	padding-top:3%;
+	padding-bottom:3%;
+}
+</style>
+<!-- whats new end -->
+<br>
+<br>
+<br>
+<br>
+<!-- footer start -->
+
+<footer id="footer">
+<div class="footer-top">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-lg-3 col-md-6 footer-contact">
+        <h3>FitPlay.<span>.</span></h3>
+        <p>
+         Kolhapur <br>
+         Maharastra<br>
+          <strong>Phone:</strong> +91 9284008321<br>
+          <strong>Email:</strong> thefitplay@gmail.com<br>
+        </p>
+
+      </div>
+
+      <div class="col-lg-3 col-md-6 footer-links">
+        <h4>Useful Links</h4>
+        <ul>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="privacy_policy.html">Privacy policy</a></li>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 footer-links">
+        <h4>Our Services</h4>
+        <ul>
+          <li><i class="bx bx-chevron-right"></i> <a href="turf.php">Book Turfs</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Explore Gym</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="shop.php">Shop Products</a></li>
+          
+        </ul>
+      </div>
+
+
+
+      <div class="col-lg-3 col-md-6 footer-links">
+        <h4>Our Social Networks</h4>
+        <p>Welcome to the heart of our vibrant community! Follow us on our social networks to stay connected with the latest in fitness trends, exciting events, exclusive promotions, and inspiring stories from our community members.</p>
+        <div class="social-links mt-3">
+          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div>
+
+      </div>
+      
+
+    </div>
+  </div>
+</div>
+
+<div class="container py-4">
+  <div class="copyright">
+    &copy; Copyright <strong><span>FitPlay</span></strong>. All Rights Reserved
+  </div>
+  
+</div>
+</footer><!-- End Footer -->
+
+<!-- footer end -->
+</body>
+</html>
