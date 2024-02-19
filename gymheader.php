666146a1cd0d1efac9d01b3ddd5aa14076ec445f
yashnikam23 <147298688+yashnikam23@users.noreply.github.com>
2024-02-19 21:35:30 +0530

    commit+

--- gymheader.php
+++ gymheader.php
@@ -462,7 +462,7 @@ function getInitials($name) {
   <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
     <div class="modal-content rounded-4 shadow">
       <div class="modal-header p-5 pb-4 border-bottom-0">
-        <h1 class="fw-bold mb-0 fs-1">Welcome Back to Fitplay.</h1>
+        <h1 class="fw-bold mb-0 fs-1" style="color: #4f4f4f;">Welcome Back to Fitplay.</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" fdprocessedid="jlo98"></button>
       </div>
       <div class="modal-body p-5 pt-0">
