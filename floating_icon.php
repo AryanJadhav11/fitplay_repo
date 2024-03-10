e0bdb40d67d37325680626ebb13fe8d670afc9e6
yashnikam23 <147298688+yashnikam23@users.noreply.github.com>
2024-03-11 01:29:22 +0530

    floating_myorder

--- floating_icon.php
+++ floating_icon.php
@@ -5,18 +5,28 @@
     <span></span>
   </div>
   <div id="floating-ligar" style="display: block;">
-    <a href="tel: 011">
-      <svg class="svg-inline--fa fa-phone fa-w-16" aria-hidden="true" data-prefix="fas" data-icon="phone" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M493.397 24.615l-104-23.997c-11.314-2.611-22.879 3.252-27.456 13.931l-48 111.997a24 24 0 0 0 6.862 28.029l60.617 49.596c-35.973 76.675-98.938 140.508-177.249 177.248l-49.596-60.616a24 24 0 0 0-28.029-6.862l-111.997 48C3.873 366.516-1.994 378.08.618 389.397l23.997 104C27.109 504.204 36.748 512 48 512c256.087 0 464-207.532 464-464 0-11.176-7.714-20.873-18.603-23.385z"></path></svg><!-- <i class="fas fa-phone"></i> -->	
+    <a href="index_home.php">
+    <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 64 64" width="64px" height="64px"><path d="M 32 4.0605469 L 31.876953 4.1835938 L 31.767578 4.0742188 L 7.921875 27.921875 L 8 28 L 8 28.060547 L 8 56 L 16 56 L 16 31.15625 L 34.71875 12.4375 L 51.990234 29.699219 L 52 56 L 56 56 L 55.990234 28.050781 L 32 4.0605469 z M 25 34 L 25 56 L 27 56 L 29 56 L 37 56 L 39 56 L 41 56 L 41 34 L 25 34 z"/></svg>	
     </a>
   </div>
   <div id="floating-whatsapp" style="display: block;">
-    <a href="https://api.whatsapp.com/send?phone=55011_________">
-      <svg class="svg-inline--fa fa-whatsapp fa-w-14" aria-hidden="true" data-prefix="fab" data-icon="whatsapp" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"></path></svg><!-- <i class="fab fa-whatsapp"></i> -->
-    </a>
-  </div>
-  <div id="floating-emergencia" data-toggle="modal" data-target="#modalUrgencia" style="display: block;">
+    <a href="myorder.php">
+    <svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 width="800px" height="800px" viewBox="0 0 948.346 948.346"
+	 xml:space="preserve">
+<g>
+	<g>
+		<path d="M457.701,939.221c0,0,0.001,0,0.001,0.002l16.472,9.123l16.472-9.123c0,0,0,0,0-0.002l246.951-136.771l158.17-87.602
+			v-20.037v-480.02v-21.83l-19.85-9.085l-47.105-21.56L474.173,0L119.534,162.316l-47.105,21.56l-19.85,9.085v21.83v480.02v20.037
+			l158.171,87.602L457.701,939.221z M586.337,328.471l-112.164,62.121L362.01,328.471L161.968,217.678L474.173,74.784
+			l312.205,142.894L586.337,328.471z M508.173,851.781V449.494l319.594-177.005v402.287L508.173,851.781z M440.173,449.494v402.288
+			L120.58,674.775V272.488L440.173,449.494z"/>
+	</g>
+</g>
+</svg>  </div>
+  <div id="floating-emergencia" data-toggle="modal" data-target="#modalUrgencia" style="display: block;"><a href="shop.php">
     <svg class="svg-inline--fa fa-plus fa-w-14" aria-hidden="true" data-prefix="fas" data-icon="plus" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg=""><path fill="currentColor" d="M448 294.2v-76.4c0-13.3-10.7-24-24-24H286.2V56c0-13.3-10.7-24-24-24h-76.4c-13.3 0-24 10.7-24 24v137.8H24c-13.3 0-24 10.7-24 24v76.4c0 13.3 10.7 24 24 24h137.8V456c0 13.3 10.7 24 24 24h76.4c13.3 0 24-10.7 24-24V318.2H424c13.3 0 24-10.7 24-24z"></path></svg><!-- <i class="fas fa-plus"></i> -->
-  </div>
+</a></div>
 </div>
 
 <div id="floating-ligar" style="display: none;">
@@ -55,7 +65,7 @@
   z-index: 99999;
   bottom: 15px;
   right: 15px;
-  background-color: #93b662;
+  background-color: #d1cf39;
   width: 50px;
   height: 50px;
   border-radius: 25px;
@@ -88,7 +98,7 @@
 #floating-ligar{
   right: 20px; 
   bottom: 20px;  
-  background-color: #93b662;
+  background-color: #dfdfdf;
   -webkit-animation: slide-ligar .75s forwards;
   -webkit-animation-delay: .75s;
   animation: slide-ligar .75s forwards;
@@ -129,7 +139,7 @@
 #floating-whatsapp{
   right: 20px;
   bottom: 20px;
-  background-color: #182c3c;
+  background-color: #54AAF2;
   -webkit-animation: slide-whatsapp .75s forwards;
   -webkit-animation-delay: .75s;
   animation: slide-whatsapp .75s forwards;
@@ -161,7 +171,7 @@
 #floating-emergencia{
   right: 20px;
   bottom: 20px;
-  background-color: #d00829;
+  background-color: #ff5050;
   -webkit-animation: slide-emergencia .75s forwards;
   -webkit-animation-delay: .75s;
   animation: slide-emergencia .75s forwards;
@@ -225,7 +235,7 @@
   float: right;
   margin-top: -20px;
   margin-right: -20px;
-  background: #93b662;
+  background: #64c2d3;
   border: none;
   color: #fff;
   font-size: 35px;
